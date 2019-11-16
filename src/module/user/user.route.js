4a74c99c461495ef51fab3ab29138da49d26f6a3
Duc Tho Tran <ducthotran2010@gmail.com>
2019-11-16 18:16:25 +0700

    test sth

--- src/module/user/user.route.js
+++ src/module/user/user.route.js
@@ -1,7 +1,11 @@
+import HTTPStatus from 'http-status';
 import { Router } from 'express';
 import * as userController from './user.controller';
 
 const routers = new Router();
+
+// test
+routers.get('/', (req, res) => res.status(HTTPStatus.OK).json('HJHJHJHJ'));
 
 routers.post('/login', userController.login);
 export default routers;
