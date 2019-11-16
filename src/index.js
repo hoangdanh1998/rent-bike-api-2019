a0e684d7101bca91e77fbe969f77103c53b681b3
Hoang Danh <hoangdanh1998>
2019-11-16 23:12:09 +0700

    change port

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 routeConfig(app);
 
-app.listen(8888, () => console.log('Server runing at port 8888'));
+app.listen(8080, () => console.log('Server runing at port 8888'));
 
 process.on('SIGINT', () => { console.log('Bye bye!'); process.exit(); });
 
