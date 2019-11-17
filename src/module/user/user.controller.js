7780f02ef909de9825f12992b6696cfe04c465a0
Hoang Danh <hoangdanh1998>
2019-11-17 16:21:04 +0700

    hhh

--- src/module/user/user.controller.js
+++ src/module/user/user.controller.js
@@ -1,12 +1,15 @@
+import mongoose from 'mongoose';
 import HTTPStatus from 'http-status';
 import userModel from './user.model';
+
+const User = mongoose.model('user', userModel);
 
 export async function login(req, res) {
   const username = req.body.username;
   const password = req.body.password;
   console.log({ username, password });
   try {
-    const user = await userModel.findOne({ username, password });
+    const user = await User.findOne({ username, password });
     if (!user) {
       console.log('T null ne');
       throw new Error('Invalid username password');
