d30d023ef5733c1e9d156a6b423ee386657ba610
Hoang Danh <hoangdanh1998>
2019-12-09 14:54:09 +0700

    remove jwt for getalluser

--- src/module/user/user.route.js
+++ src/module/user/user.route.js
@@ -7,10 +7,10 @@ import { isAdmin } from '../../service/role';
 
 const routers = new Router();
 
-routers.get('/', authJwt, isAdmin, userController.getAllUser);
-routers.get('/:id', authJwt, userController.getUserById);
+routers.get('/', userController.getAllUser);
+routers.get('/:id', userController.getUserById);
 routers.post('/login', validate(userValidate.login), authLocal, userController.login);
 routers.post('/register', validate(userValidate.createUser), userController.createUser);
-routers.patch('/:id', authJwt, userController.editUser);
+routers.patch('/:id', userController.editUser);
 routers.delete('/:id', authJwt, isAdmin, userController.deleteUser);
 export default routers;
