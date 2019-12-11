7ae80b75e127b0ebf8b0c89ad78d4656a4b39fed
Hoang Danh <hoangdanh1998>
2019-12-11 21:59:45 +0700

    getallbike

--- src/module/bike/bike.controller.js
+++ src/module/bike/bike.controller.js
@@ -1,6 +1,7 @@
 import httpStatus from 'http-status';
 import Bike from './bike.model';
 import constants from '../../config/constants';
+import Booking from '../booking/booking.model';
 
 export const getAllBike = async (req, res) => {
   const limit = parseInt(req.query.limit, 10) || 50;
@@ -21,12 +22,23 @@ export const getAllBikeFilter = async (req, res) => {
   const skip = parseInt(req.query.skip, 10) || 0;
   const status = constants.BIKESTATUS.AVAILABLE;
   const pickUpDate = req.query.pickUpDate;
+  const returnDate = req.query.returnDate;
   const transmisstionType = req.query.transmisstionType;
+  
   try {
-    const listBike = await Bike.find({ bikeStatus: status }).skip(skip).limit(limit).sort({ moneyRent: 1 })
+    const listBike = await Bike.find({ bikeStatus: status, transmisstionType }).skip(skip).limit(limit).sort({ moneyRent: 1 })
       .populate('branch');
-    const total = await Bike.count({ bikeStatus: status });
-    return res.status(httpStatus.OK).json({ listBike, total });
+    
+    const listBookingAvailable = await Booking.find({ 
+      $or: [
+        { pickUpDate: { $gt: returnDate } },
+        { returnDate: { $lt: pickUpDate } },
+      ],
+    }, { bike: 1 }).populate('bike');
+    console.log(listBookingAvailable);
+    // const total = listBike.length;
+    return res.status(httpStatus.OK);
+    // .json({ listBike, total })
   } catch (err) {
     return res.status(httpStatus.BAD_REQUEST).json(err.message);
   }
