4ba1431d4fea2bb3b0c2b6c68cc4401547981ed9
Hoang Danh <hoangdanh1998>
2019-12-16 20:25:15 +0700

    SWD closed

--- src/module/branch/branch.controller.js
+++ src/module/branch/branch.controller.js
@@ -1,27 +1,27 @@
-// import redis from 'redis';
+import redis from 'redis';
 import httpStatus from 'http-status';
 import Branch from './branch.model';
 import Bike from '../bike/bike.model';
 
-// const client = redis.createClient(process.env.REDISCLOUD_URL);
-// client.on('error', (err) => {
-//   console.log(`Error ${err}`);
-// });
-// export const getAllBranch = (req, res) => {
-//   try {
-//     const branchRedisKey = 'redis:branch';
-//     return client.get(branchRedisKey, async (err, listBranch) => {
-//       if (listBranch) {
-//         return res.status(httpStatus.OK).json({ scoure: 'cache', listBranches: listBranch });
-//       }
-//       const listBranches = await Branch.find({});
-//       client.setex(branchRedisKey, 3600, JSON.stringify(listBranches));
-//       return res.status(httpStatus.OK).json({ scoure: 'db', listBranches });
-//     });
-//   } catch (err) {
-//     return res.status(httpStatus.BAD_REQUEST).json(err.message);
-//   }
-// };
+const client = redis.createClient(process.env.REDISCLOUD_URL);
+client.on('error', (err) => {
+  console.log(`Error ${err}`);
+});
+export const getAllBranch = (req, res) => {
+  try {
+    const branchRedisKey = 'redis:branch';
+    return client.get(branchRedisKey, async (err, listBranch) => {
+      if (listBranch) {
+        return res.status(httpStatus.OK).json({ scoure: 'cache', listBranches: listBranch });
+      }
+      const listBranches = await Branch.find({});
+      client.setex(branchRedisKey, 3600, JSON.stringify(listBranches));
+      return res.status(httpStatus.OK).json({ scoure: 'db', listBranches });
+    });
+  } catch (err) {
+    return res.status(httpStatus.BAD_REQUEST).json(err.message);
+  }
+};
 export const getAllBranchNoCache = async (req, res) => {
   try {
     const listBranches = await Branch.find({});
