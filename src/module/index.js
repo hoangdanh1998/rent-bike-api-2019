764bdad6e27ac9dc40311e4cf05335d1b4d9e201
Nguyen Hoang Danh <nguyenhoangdanh@Danh-MacBook.local>
2020-09-28 09:11:22 +0700

    hello world

--- src/module/index.js
+++ src/module/index.js
@@ -1,15 +1,16 @@
-import userRouters from './user/user.route';
-import bikeRouters from './bike/bike.route';
-import branchRouters from './branch/branch.route';
-import bookingRouters from './booking/booking.route';
-import serviceRouters from '../service/firebase';
+import userRouters from "./user/user.route";
+import bikeRouters from "./bike/bike.route";
+import branchRouters from "./branch/branch.route";
+import bookingRouters from "./booking/booking.route";
+import serviceRouters from "../service/firebase";
 // import redisRouters from '../service/redis/redis-location.controller';
 
-export default app => {
-  app.use('/users', userRouters);    
-  app.use('/bikes', bikeRouters);
-  app.use('/branches', branchRouters);
-  app.use('/bookings', bookingRouters);
-  app.use('/service/google', serviceRouters);
+export default (app) => {
+  app.use("/", (req, res) => res.send("hello world"));
+  app.use("/users", userRouters);
+  app.use("/bikes", bikeRouters);
+  app.use("/branches", branchRouters);
+  app.use("/bookings", bookingRouters);
+  app.use("/service/google", serviceRouters);
   // app.use('/location', redisRouters);
 };
